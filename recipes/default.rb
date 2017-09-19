6cf94c589c5167883619075229d7b77aa8c5f9d2
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-19 16:14:53 +0100

    done

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,33 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+apt_update
+
+package 'nginx'
+
+service 'nginx' do 
+	supports status: true, restart: true, reload: true
+	action [:enable, :start]
+end
+
+template '/etc/nginx/sites-available/default' do 
+	source 'nginx.default.erb'
+	notifies :reload, "service[nginx]"
+end 
+
+remote_file "/tmp/nodesource_setup.sh" do
+	source "https://deb.nodesource.com/setup_6.x"
+	action :create
+end
+
+execute "update node resources" do
+	command "sh /tmp/nodesource_setup.sh"
+end
+
+package "nodejs" do
+	action :upgrade
+end
+
+execute "install npm" do 
+	command "npm install -g pm2"
+end
